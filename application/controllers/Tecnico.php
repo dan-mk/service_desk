210acc40cc3103e6c56e17556a4f4d1f32276917
dan-mk <danielmk98@hotmail.com>
2015-12-10 16:38:30 -0300

    Início técnico

---
+++ application/controllers/Tecnico.php
@@ -0,0 +1,23 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tecnico extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        if ($this->is_logged()) {
+			$this->load->view("core/head");
+			$data["logged"] = $this->is_logged();
+			$data["admin"] = $this->is_admin();
+			$this->load->view("core/cabecalho", $data);
+			$this->load->view("tecnico/index", $data);
+        } else {
+            redirect("logon");
+        }
+    }
+
+}
